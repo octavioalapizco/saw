64f1991c7fa0b00ab125fa83702d57020c53a6b9
Zesar Bibriesca <runtim3.error@gmail.com>
2012-05-15 16:52:48 -0600

    ...

--- mvc/controladores/home.php
+++ mvc/controladores/home.php
@@ -1,12 +1,13 @@
 <?php
 require_once('../mvc.php');
 
-require_once('../vistas/theme1/home_view.php');
-
 $home=new HomeController();
 $home->index();
+
+
 class HomeController{
 	function index(){	
+		require_once('../vistas/theme1/home_view.php');
 		$tema= new Theme();
 		$vista=new HomeView();
 		$tema->setVista($vista);
--- mvc/controladores/nosotros.php
+++ mvc/controladores/nosotros.php
@@ -1,11 +1,12 @@
 <?php
 require_once('../mvc.php');	
-require_once('../vistas/theme1/nosotros_view.php');
 
 $nosotros=new NosotrosController();
 $nosotros->index();
+
 class NosotrosController{
 	function index(){	
+		require_once('../vistas/theme1/nosotros_view.php');
 		$tema= new Theme();
 		$vista=new NosotrosView();
 		$tema->setVista($vista);
