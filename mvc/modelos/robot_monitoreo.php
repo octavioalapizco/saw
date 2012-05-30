d7e14a5d40c2d55958ae963463bac029c628e689
colab_01.diem <runtim3.error@gmail.com>
2012-05-29 22:31:33 -0600

    ...

--- mvc/modelos/robot_monitoreo.php
+++ mvc/modelos/robot_monitoreo.php
@@ -7,8 +7,40 @@ El robot tiene dos tareas principales:
 */
 class MonitoreoRobot extends Model{
 	
-	private function actualizarEstadoDeDispositivos(){
+	public function actualizar(){
+		$this->actualizarProgramacionDiaria();
+		//$this->actualizarEstadoDeDispositivos();
+		return true;
+	}
+	
+	private function actualizarEstadoDeDispositivos(){	
+		//en base a la hora se determina cuales dispositivos deben cambiar de estado
+		//despues de haber cambiado el estado a los dispositivos (en la bd), se envian los 
+		//comandos necesarios para actualizar el estado del hardware
 		
+		$dbh=$this->getDb();		
+		#----------------------------------------------------
+		#	Actualiza la tabla de programacion_del_dia
+		#----------------------------------------------------
+		
+		$sql = 'UPDATE programacion_del_dia SET estado="APAGADO" WHERE estado="ENCENDIDO" AND now() IS NOT BETWEEN fechaInicio AND  fechaFin ';
+		$sth = $dbh->prepare($sql);		
+		$sth->execute();
+		
+		$sql = 'UPDATE programacion_del_dia SET estado="ACTIVO" where now() BETWEEN fechaInicio AND  fechaFin AND estado!="CANCELADO"';		
+		$sth = $dbh->prepare($sql);				
+		$sth->execute();
+		
+		#----------------------------------------------------
+		#	Actualiza la tabla dispositivos
+		#----------------------------------------------------
+		$sql='UPDATE dispositivos SET estado="OFF" WHERE eventoCancelado!=true';
+		$sth = $dbh->prepare($sql);				
+		$sth->execute();
+		
+		$sql='UPDATE dispositivos SET estado="ON" WHERE idDispositivo IN (SELECT dispositivoId FROM programacion_del_dia WHERE estado ="ACTIVO" AND tipo="ON")';
+		$sth = $dbh->prepare($sql);				
+		$sth->execute();
 	}
 	
 	private function actualizarProgramacionDiaria(){
@@ -17,22 +49,18 @@ class MonitoreoRobot extends Model{
 		$sql='SELECT DATE_FORMAT(now(),"%Y-%m-%d") as fechaNow,fecha_actual fechaActual FROM config limit 0,1';
 		$sth = $dbh->prepare($sql);		
 		$sth->execute();
-		$fechas = $sth->fetchAll(PDO::FETCH_ASSOC);		
+		$fechas = $sth->fetchAll(PDO::FETCH_ASSOC);
 		
 		if ($fechas[0]['fechaNow'] == $fechas[0]['fechaActual']){
 			return true;						
 		}
-		//Llegar a este punto significaa que el dia ya ha cambiado, entonces es necesario actualizar la programacion del dia		
-		
-		
+		//Llegar a este punto significaa que el dia ya ha cambiado, entonces es necesario actualizar la programacion del dia						
 		$date = DateTime::CreateFromFormat("Y-m-d",$fechas[0]['fechaNow']);		
 		$dia_de_la_semana= $date->format('N');	//1=Lunes...7=Domingo
 		
-		$dbh->beginTransaction();//startTransaction
-				
-		//Se borra el contenido de la tabla programacion_del_dia
+		$dbh->beginTransaction();//startTransaction				
 		
-		
+		//Se borra el contenido de la tabla programacion_del_dia				
 		$sql='TRUNCATE TABLE programacion_del_dia';
 		$sth = $dbh->prepare($sql);		
 		$sth->execute();
@@ -44,9 +72,12 @@ class MonitoreoRobot extends Model{
 		$sth->execute();
 		
 		//Se agregan de la tabla programacion_semanal, los registros correspondientes al dia de la semana
-		
+	
 		$sql='INSERT INTO programacion_del_dia (dispositivoId,tipo,nombre,fechaInicio,fechaFin,estado)
-		SELECT dispositivoId,tipo,nombre,fechaInicio,fechaFin,estado FROM programacion_semanal WHERE dia=:dia';
+		SELECT dispositivoId,tipo,nombre,CONCAT(DATE_FORMAT("'.$fechas[0]['fechaNow'].'","%Y-%m-%d"), DATE_FORMAT(fechaInicio," %H:%i:%s")) ,CONCAT(DATE_FORMAT("'.$fechas[0]['fechaNow'].'","%Y-%m-%d"), DATE_FORMAT(fechaFin," %H:%i:%s")),estado FROM programacion_semanal WHERE dia=:dia';
+		
+		/*$sql='INSERT INTO programacion_del_dia (dispositivoId,tipo,nombre,fechaInicio,fechaFin,estado)
+		SELECT dispositivoId,tipo,nombre,fechaInicio,fechaFin,estado FROM programacion_semanal WHERE dia=:dia';*/
 		
 		$sth = $dbh->prepare($sql);		
 		$sth->bindValue(':dia',intval($dia_de_la_semana),PDO::PARAM_INT);	
@@ -64,7 +95,7 @@ class MonitoreoRobot extends Model{
 		//$sth->execute();
 	}
 	
-	public function actualizar(){
+	/*public function actualizar(){
 		$this->actualizarProgramacionDiaria();
 		return true;
 		#----------------------------------------------------		
@@ -101,7 +132,7 @@ class MonitoreoRobot extends Model{
 		$horarios = $sth->fetchAll(PDO::FETCH_ASSOC);
 		return $horarios;	
 	
-	}
+	}*/
 }
 
 ?>
